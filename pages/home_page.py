8f6b4fc583d1129487a54c14b50e26887d6a4361
andreibnc <61779930+andreibnc@users.noreply.github.com>
2020-03-18 11:38:06 +0200

    added test for four column component on the home page

--- pages/home_page.py
+++ pages/home_page.py
@@ -36,7 +36,53 @@ class HomePage(BasePage):
     # --------------------------------------------------------------------------------------------------------------------------------
     # Four Column Promo Component
 
-    four_col_component = {"by": By.CSS_SELECTOR, "value": 'section.multi-col-press-release'}
+    four_column_component = {"by": By.CSS_SELECTOR, "value": 'body > section:nth-child(5)'}
+
+    four_column_component_1 = {"by": By.CSS_SELECTOR,
+                               "value": 'section:nth-child(4) > div > div > ul > li:nth-child(1)'}
+
+    four_column_component_1_image = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(1) > a'}
+    four_column_component_1_name = {"by": By.CSS_SELECTOR,
+                                    "value": 'section:nth-child(4) > div > div > ul > li:nth-child(1) > div > a > h3'}
+    four_column_component_1_title = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(1) > div > h4'}
+    four_column_component_1_description = {"by": By.CSS_SELECTOR,
+                                           "value": 'section:nth-child(4) > div > div > ul > li:nth-child(1) > div > p'}
+    four_column_component_2 = {"by": By.CSS_SELECTOR,
+                               "value": 'section:nth-child(4) > div > div > ul > li:nth-child(2)'}
+
+    four_column_component_2_image = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(2) > a'}
+    four_column_component_2_name = {"by": By.CSS_SELECTOR,
+                                    "value": 'section:nth-child(4) > div > div > ul > li:nth-child(2) > div > a > h3'}
+    four_column_component_2_title = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(2) > div > h4'}
+    four_column_component_2_description = {"by": By.CSS_SELECTOR,
+                                           "value": 'section:nth-child(4) > div > div > ul > li:nth-child(2) > div > p'}
+    four_column_component_3 = {"by": By.CSS_SELECTOR,
+                               "value": 'section:nth-child(4) > div > div > ul > li:nth-child(3)'}
+
+    four_column_component_3_image = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(3) > a'}
+    four_column_component_3_name = {"by": By.CSS_SELECTOR,
+                                    "value": 'section:nth-child(4) > div > div > ul > li:nth-child(3) > div > a > h3'}
+    four_column_component_3_title = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(3) > div > h4'}
+    four_column_component_3_description = {"by": By.CSS_SELECTOR,
+                                           "value": 'section:nth-child(4) > div > div > ul > li:nth-child(3) > div > p'}
+    four_column_component_4 = {"by": By.CSS_SELECTOR,
+                               "value": 'section:nth-child(4) > div > div > ul > li:nth-child(4)'}
+
+    four_column_component_4_image = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(4) > a'}
+    four_column_component_4_name = {"by": By.CSS_SELECTOR,
+                                    "value": 'section:nth-child(4) > div > div > ul > li:nth-child(4) > div > a > h3'}
+    four_column_component_4_title = {"by": By.CSS_SELECTOR,
+                                     "value": 'section:nth-child(4) > div > div > ul > li:nth-child(4) > div > h4'}
+    four_column_component_4_description = {"by": By.CSS_SELECTOR,
+                                           "value": 'section:nth-child(4) > div > div > ul > li:nth-child(4) > div > p'}
+
     def navigate_to_slb(self):
         self._visit(baseurl)
 
@@ -78,3 +124,86 @@ class HomePage(BasePage):
 
     def return_current_url(self):
         return self._get_current_url()
+
+    # --------------------------------------------------------------------------------------------------------------------------------
+    # Four Column Promo Component
+    def scroll_to_four_column_component(self):
+        return self._scroll_to_element(self.four_column_component)
+
+    def four_column_component_is_displayed(self):
+        return self._is_displayed(self.four_column_component)
+
+    def four_column_component_1_is_displayed(self):
+        return self._is_displayed(self.four_column_component_1)
+
+    def four_column_component_2_is_displayed(self):
+        return self._is_displayed(self.four_column_component_2)
+
+    def four_column_component_3_is_displayed(self):
+        return self._is_displayed(self.four_column_component_3)
+
+    def four_column_component_4_is_displayed(self):
+        return self._is_displayed(self.four_column_component_4)
+
+    def four_column_component_1_image_is_displayed(self):
+        return self._is_displayed(self.four_column_component_1_image)
+
+    def four_column_component_2_image_is_displayed(self):
+        return self._is_displayed(self.four_column_component_2_image)
+
+    def four_column_component_3_image_is_displayed(self):
+        return self._is_displayed(self.four_column_component_3_image)
+
+    def four_column_component_4_image_is_displayed(self):
+        return self._is_displayed(self.four_column_component_4_image)
+
+    def four_column_component_1_name_get_text(self):
+        return self._get_text(self.four_column_component_1_name)
+
+    def four_column_component_2_name_get_text(self):
+        return self._get_text(self.four_column_component_2_name)
+
+    def four_column_component_3_name_get_text(self):
+        return self._get_text(self.four_column_component_3_name)
+
+    def four_column_component_4_name_get_text(self):
+        return self._get_text(self.four_column_component_4_name)
+
+    def four_column_component_1_title_get_text(self):
+        return self._get_text(self.four_column_component_1_title)
+
+    def four_column_component_2_title_get_text(self):
+        return self._get_text(self.four_column_component_2_title)
+
+    def four_column_component_3_title_get_text(self):
+        return self._get_text(self.four_column_component_3_title)
+
+    def four_column_component_4_title_get_text(self):
+        return self._get_text(self.four_column_component_4_title)
+
+    def four_column_component_1_description_get_text(self):
+        return self._get_text(self.four_column_component_1_description)
+
+    def four_column_component_2_description_get_text(self):
+        return self._get_text(self.four_column_component_2_description)
+
+    def four_column_component_3_description_get_text(self):
+        return self._get_text(self.four_column_component_3_description)
+
+    def four_column_component_4_description_get_text(self):
+        return self._get_text(self.four_column_component_4_description)
+
+    def get_current_link(self):
+        return self._get_current_url()
+
+    def click_on_four_column_component_1_image(self):
+        self._click(self.four_column_component_1_image)
+
+    def click_on_four_column_component_2_image(self):
+        self._click(self.four_column_component_2_image)
+
+    def click_on_four_column_component_3_image(self):
+        self._click(self.four_column_component_3_image)
+
+    def click_on_four_column_component_4_image(self):
+        self._click(self.four_column_component_4_image)
--- tests/home_page_test.py
+++ tests/home_page_test.py
@@ -13,7 +13,7 @@ class TestHomePage():
     def test_press_release_component(self, driver, home):
         home.navigate_to_slb()
         time.sleep(2)
-        current_link = "https://www.slb.com/who-we-are/hse/covid-179"
+        current_link = "https://www.slb.com/who-we-are/hse/covid-19"
 
         if home.press_release_component_is_displayed():
             print(home.press_release_1_get_date() + '  ' + home.press_release_1_get_text())
@@ -33,6 +33,80 @@ class TestHomePage():
                     print("The CTA button is not displayed")
             else:
                 print("The callout component is not available")
-
         else:
             print("The press release component is not present on the page")
+
+    def test_four_column_component(self, driver, home):
+        four_column_component_1_link = 'https://www.slb.com/drilling/surface-and-downhole-logging/measurements-while-drilling-services/xbolt-g2-accelerated-drilling-service'
+        four_column_component_2_link = 'https://www.slb.com/completions/well-completions/packers/blueplug-max-p-bridge-plug'
+        four_column_component_3_link = 'https://www.slb.com/completions/stimulation/frac-and-flowback-equipment/valvecommander-platform'
+        four_column_component_4_link = 'https://www.slb.com/well-intervention/coiled-tubing-intervention/active-real-time-downhole-coiled-tubing-services/active-power-ct-powered-measurements-system'
+
+        home.navigate_to_slb()
+        time.sleep(2)
+        home.scroll_to_four_column_component()
+        time.sleep(2)
+
+        if home.four_column_component_is_displayed():
+            if home.four_column_component_1_is_displayed():
+                print("-------------------------------------------------")
+                print(home.four_column_component_1_name_get_text())
+                print(home.four_column_component_1_title_get_text())
+                print(home.four_column_component_1_description_get_text())
+                print("-------------------------------------------------")
+                if home.four_column_component_1_image_is_displayed():
+                    home.click_on_four_column_component_1_image()
+                    time.sleep(2)
+                    assert (four_column_component_1_link == home.get_current_link())
+                    home.navigate_to_slb()
+                else:
+                    print("The link has been changed")
+            else:
+                print("The Component 1 is not displayed")
+
+            if home.four_column_component_2_is_displayed():
+                print(home.four_column_component_2_name_get_text())
+                print(home.four_column_component_2_title_get_text())
+                print(home.four_column_component_2_description_get_text())
+                print("-------------------------------------------------")
+                if home.four_column_component_2_image_is_displayed():
+                    home.click_on_four_column_component_2_image()
+                    time.sleep(2)
+                    assert (four_column_component_2_link == home.get_current_link())
+                    home.navigate_to_slb()
+                else:
+                    print("The link has been changed")
+            else:
+                print("The Component 2 is not displayed")
+
+            if home.four_column_component_3_is_displayed():
+                print(home.four_column_component_3_name_get_text())
+                print(home.four_column_component_3_title_get_text())
+                print(home.four_column_component_3_description_get_text())
+                print("-------------------------------------------------")
+                if home.four_column_component_3_image_is_displayed():
+                    home.click_on_four_column_component_3_image()
+                    time.sleep(2)
+                    assert (four_column_component_3_link == home.get_current_link())
+                    home.navigate_to_slb()
+                else:
+                    print("The link has been changed")
+            else:
+                print("The Component 3 is not displayed")
+            if home.four_column_component_4_is_displayed():
+                print(home.four_column_component_4_name_get_text())
+                print(home.four_column_component_4_title_get_text())
+                print(home.four_column_component_4_description_get_text())
+                print("-------------------------------------------------")
+                if home.four_column_component_4_image_is_displayed():
+                    home.click_on_four_column_component_4_image()
+                    time.sleep(2)
+                    assert (four_column_component_4_link == home.get_current_link())
+                    home.navigate_to_slb()
+                else:
+                    print("The link has been changed")
+            else:
+                print("The Component 4 is not displayed")
+
+        else:
+            print("The Four Column Component Is Not Displayed")
